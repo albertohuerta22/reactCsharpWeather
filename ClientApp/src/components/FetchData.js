933c17c8f4a6cbeaaaaccccc916f0354b6fa990f
albertohuerta <albertohuerta22@gmail.com>
2023-10-17 18:11:13 -0400

    converted counter comp to functional, added logger, controller works

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,63 +1,64 @@
 import React, { useState, useEffect } from 'react';
 
 const FetchData = () => {
-    const [forecasts, setForecasts] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [forecasts, setForecasts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const populateWeatherData = async () => {
-            try {
-                const response = await fetch('weatherforecast');
-                const data = await response.json();
-                setForecasts(data);
-                setLoading(false);
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const populateWeatherData = async () => {
+      try {
+          const response = await fetch("weatherforecast");
+          const data = await response.json();
+        console.log(data)
+        setForecasts(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setLoading(false);
+      }
+    };
 
-        populateWeatherData();
-    }, []);
+    populateWeatherData();
+  }, []);
 
-    const renderForecastsTable = (forecasts) => (
-        <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map((forecast) => (
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
+  const renderForecastsTable = (forecasts) => (
+    <table className="table table-striped" aria-labelledby="tableLabel">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Temp. (C)</th>
+          <th>Temp. (F)</th>
+          <th>Summary</th>
+        </tr>
+      </thead>
+      <tbody>
+        {forecasts.map((forecast) => (
+          <tr key={forecast.date}>
+            <td>{forecast.date}</td>
+            <td>{forecast.temperatureC}</td>
+            <td>{forecast.temperatureF}</td>
+            <td>{forecast.summary}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
-    const contents = loading ? (
-        <p>
-            <em>Loading...</em>
-        </p>
-    ) : (
-        renderForecastsTable(forecasts)
-    );
+  const contents = loading ? (
+    <p>
+      <em>Loading...</em>
+    </p>
+  ) : (
+    renderForecastsTable(forecasts)
+  );
 
-    return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
+  return (
+    <div>
+      <h1 id="tableLabel">Weather forecast</h1>
+      <p>This component demonstrates fetching data from the server.</p>
+      {contents}
+    </div>
+  );
 };
 
 export default FetchData;

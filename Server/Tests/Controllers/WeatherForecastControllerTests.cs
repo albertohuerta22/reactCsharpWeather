59bd08acb56c1ad03134afb14e2eaa1f8d4dd6f1
albertohuerta <albertohuerta22@gmail.com>
2023-10-18 12:40:49 -0400

    created simple logger for weatherforecast test

--- Server/Tests/Controllers/WeatherForecastControllerTests.cs
+++ Server/Tests/Controllers/WeatherForecastControllerTests.cs
@@ -1,11 +1,45 @@
-﻿using System;
-namespace ReactTestApp.Server.Tests.Controllers
-{
-	public class EmptyClass
-	{
-		public EmptyClass()
-		{
-		}
-	}
-}
+﻿using Microsoft.AspNetCore.Routing;
+using NUnit.Framework;
+using ReactTestApp.Controllers;
+using Moq;
+using Microsoft.Extensions.Logging;
+//using Internal;
+using System;
 
+namespace ReactTestApp.Tests.Controllers
+{
+    [TestFixture]
+    public class WeatherForecastControllerTests
+    {
+        [Test]
+        public void Get_ReturnsCorrectData()
+        {
+            // Arrange
+            var loggerMock = new MockLogger<WeatherForecastController>();
+            var controller = new WeatherForecastController(loggerMock);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsNotNull(result);
+            // Add more specific assertions based on your requirements
+
+            
+        }
+
+        // Add more test methods as needed
+    }
+    // Simple implementation of ILogger<T> for testing purposes
+    public class MockLogger<T> : ILogger<T>
+    {
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            // Do nothing for testing purposes
+        }
+    }
+}

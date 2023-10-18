dc4c00f93c926406b7396c9ee01ae2f4d7de8b9b
albertohuerta <albertohuerta22@gmail.com>
2023-10-17 21:15:06 -0400

    adjusted route reference, created server, controller unit test file

---
+++ Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReactTestApp.Controllers;
+
+[ApiController]
+[Route("weatherforecast")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        _logger.LogInformation("Get method called.");
+
+        var data = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+
+        _logger.LogInformation("Returning data: {@Data}", data);
+
+        return data;
+    }
+
+}
+

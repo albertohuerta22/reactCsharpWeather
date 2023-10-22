155c0744a6d2793747ddb029fed37ce57bf794fb
albertohuerta <albertohuerta22@gmail.com>
2023-10-21 22:35:48 -0400

    created registration page

---
+++ ClientApp/src/components/Registration.jsx
@@ -0,0 +1,97 @@
+﻿import React, { useState } from 'react';
+
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Perform validation here if needed
+
+        // Assuming you have a function to handle registration
+        registerUser(formData);
+    };
+
+    return (
+        <div>
+            <h2>Registration</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="firstName">First Name:</label>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        value={formData.firstName}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name:</label>
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        value={formData.lastName}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Register</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+// Assuming you have a function to handle registration
+const registerUser = (userData) => {
+    // Implement the logic to send registration data to the server
+    console.log('Registering user:', userData);
+};
+
+export default RegistrationForm;

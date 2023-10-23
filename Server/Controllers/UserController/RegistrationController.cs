515d86017a0f044464a90d05c7539451f0b4b04b
albertohuerta <albertohuerta22@gmail.com>
2023-10-23 16:53:09 -0400

    changed register to usercontroller folder, adjusted usercontroller

---
+++ Server/Controllers/UserController/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReactTestApp.Controllers
+{
+    [ApiController]
+    [Route("api/users")] // You can adjust the route as needed
+    public class UserController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UserController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
